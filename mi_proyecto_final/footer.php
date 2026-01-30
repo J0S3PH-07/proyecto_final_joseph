e0c2e6acdfd6d679639906302dfcb3508feca04b
J0S3PH-07 <2223_joseph.abanto@iticbcn.cat>
2026-01-30 22:11:55 +0100

    Delete mi_proyecto_final directory

--- mi_proyecto_final/footer.php
+++
@@ -1,8 +0,0 @@
-    </main>
-    <footer>
-        <div class="container">
-            <p>&copy; <?php echo date("Y"); ?> Gestión de Biblioteca. Todos los derechos reservados.</p>
-        </div>
-    </footer>
-</body>
-</html>
